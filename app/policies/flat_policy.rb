55bef6bd56ac026dc8c6f9fa1bf82673ca42f666
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-24 16:06:01 -0300

    adds admin user

--- app/policies/flat_policy.rb
+++ app/policies/flat_policy.rb
@@ -5,7 +5,8 @@ class FlatPolicy < ApplicationPolicy
     #   scope.all
     # end
     def resolve
-      scope.all # If users can see all restaurants
+      scope.all
+      # If users can see all restaurants
       # scope.where(user: user) # If users can only see their restaurants
       # scope.where("name LIKE 't%'") # If users can only see restaurants starting with `t`
       # ...
@@ -25,12 +26,18 @@ class FlatPolicy < ApplicationPolicy
   end
 
   def update?
-    record.user == user
+    is_user_or_admin?
     # record: the restaurant passed to the `authorize` method in controller
     # user: the `current_user` signed in with Devise
   end
 
   def destroy?
-    record.user == user
+    is_user_or_admin?
+  end
+
+  private
+
+  def is_user_or_admin?
+    record.user == user || user.admin
   end
 end
--- app/policies/reservation_policy.rb
+++ app/policies/reservation_policy.rb
@@ -5,7 +5,8 @@ class ReservationPolicy < ApplicationPolicy
     #   scope.all
     # end
     def resolve
-      scope.where(user: user) # If users can see all restaurants
+      user.admin? ? scope.all : scope.where(user: user)
+       # If users can see all restaurants
       # scope.where(user: user) # If users can only see their restaurants
       # scope.where("name LIKE 't%'") # If users can only see restaurants starting with `t`
       # ...
@@ -17,7 +18,7 @@ class ReservationPolicy < ApplicationPolicy
   end
 
   def show?
-    record.user == user
+    is_user_or_admin?
   end
 
   def create?
@@ -25,12 +26,18 @@ class ReservationPolicy < ApplicationPolicy
   end
 
   def update?
-    record.user == user
+    is_user_or_admin?
     # record: the restaurant passed to the `authorize` method in controller
     # user: the `current_user` signed in with Devise
   end
 
   def destroy?
-    record.user == user
+    is_user_or_admin?
+  end
+
+  private
+
+  def is_user_or_admin?
+    record.user == user || user.admin
   end
 end
