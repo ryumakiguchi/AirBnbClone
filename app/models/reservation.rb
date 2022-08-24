767aa70b4d63393740760bc73b2b81f4be319ca4
Victor Sa <sa.victor.cp@gmail.com>
2022-08-24 18:34:31 -0300

    Home cards

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :flat
   belongs_to :user
+  validates :check_in, :check_out, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_150307) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_195531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,8 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_150307) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.date "check_in"
-    t.date "check_out"
+    t.datetime "check_in", precision: nil
+    t.datetime "check_out", precision: nil
     t.boolean "payment_status", default: false
     t.string "confirmation_status", default: "pending"
     t.bigint "flat_id", null: false
