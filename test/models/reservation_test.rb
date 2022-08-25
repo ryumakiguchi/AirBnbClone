d379cec70f5282be57e8af03c28f3b7a12177480
Eduardo Aguilar <laloaps@gmail.com>
2022-08-24 21:34:54 -0400

    Add checkin before checkout validation in reservation

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ReservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid if checkout date is earlier than checkin date" do
+    user = users(:mew_two)
+    flat = flats(:veracruz)
+    date_today = DateTime.now
+    date_yesterday = date_today - 1
+    reservation = Reservation.new(flat: flat, user: user, check_in: date_today, check_out: date_yesterday)
+    refute reservation.valid?
+  end
+
+  test "invalid if checkout date is same date as checkin date" do
+    user = users(:mew_two)
+    flat = flats(:veracruz)
+    date_today = DateTime.now
+    reservation = Reservation.new(flat: flat, user: user, check_in: date_today, check_out: date_today)
+    refute reservation.valid?
+  end
 end
