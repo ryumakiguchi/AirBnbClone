5bbfd0a648ab3efec5e126aa2ce0f968d763a9b9
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-25 16:33:47 -0300

    style user forms, added column first_name, last_name and pirctue url with forms modifications

---
+++ db/migrate/20220825183320_add_name_last_name_picture_url_to_user.rb
@@ -0,0 +1,7 @@
+class AddNameLastNamePictureUrlToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :picture_url, :string
+  end
+end
