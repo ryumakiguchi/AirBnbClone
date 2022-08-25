193939162f342676d18b531ffd56c7555b17e6d2
Eduardo Aguilar <laloaps@gmail.com>
2022-08-24 21:43:01 -0400

    Validate check-in can't be in the past

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
 class ReservationTest < ActiveSupport::TestCase
+  test "invalid if checkin date is before today" do
+    user = users(:mew_two)
+    flat = flats(:veracruz)
+    date_today = DateTime.now
+    reservation = Reservation.new(flat: flat, user: user, check_in: date_today - 1, check_out: date_today + 4)
+    refute reservation.valid?
+  end
+
   test "invalid if checkout date is earlier than checkin date" do
     user = users(:mew_two)
     flat = flats(:veracruz)
