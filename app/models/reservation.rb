af8e9d723f874502a74327155b90436965441937
Eduardo Aguilar <laloaps@gmail.com>
2022-08-24 21:34:54 -0400

    Add checkin before checkout validation in reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :flat
   belongs_to :user
   validates :check_in, :check_out, presence: true
+  validates :check_out, comparison: { greater_than: :check_in }
 end
